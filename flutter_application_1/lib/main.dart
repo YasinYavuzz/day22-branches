01e65b73f90c6f925e5355aec92858c9ea2149a9
yasinyavuz <yavuzyasin.eem@gmail.com>
2022-10-06 12:13:48 +0300

    add bugBranchVariable

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -50,6 +50,7 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
   int developBranchVariable = 0;
+  int bugBranchVariable = 0;
 
   void _incrementCounter() {
     setState(() {
